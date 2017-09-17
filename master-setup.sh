4b6d907d2e64605f4d1039008974ac337566a73b
Hagai Barel <hagaib@codevalue.net>
2017-09-17 15:25:21 +0300

    fixed master script to force copy config file to vagrant folder

--- master-setup.sh
+++ master-setup.sh
@@ -7,12 +7,13 @@ sudo kubeadm init --apiserver-advertise-address $1 --token $2
 
 # move kubecfg file to home folder
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp -rf /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # install weave network plugin 
+# https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/#pod-network
 export kubever=$(kubectl version | base64 | tr -d '\n')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever" 
 
 # copy kubecfg file to shared vagrant directory
-cp -i $HOME/.kube/config /vagrant/config
+cp -rf $HOME/.kube/config /vagrant/config
