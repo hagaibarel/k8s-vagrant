c898f361e52b2cf4255e21518a975050d240571a
Hagai Barel <hagaib@codevalue.net>
2017-10-01 10:37:20 +0300

    Update Docker to CE version 17.03

--- install.sh
+++ install.sh
@@ -1,31 +1,30 @@
 #!/bin/sh
 
 export KUBEVER=1.8.0-00
-export DOCKERVER=1.12.6-0~ubuntu-xenial
+export DOCKERVER=17.03.2~ce-0~ubuntu-xenial
 
 apt-get update && apt-get install -yq apt-transport-https \
   linux-image-extra-$(uname -r) \
   linux-image-extra-virtual \
   ca-certificates
 
-#add kubernetes gpg repo key
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+#add kubernetes apt repository and gpg key
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
-#add docker gpg repo key
-apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-  --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+add-apt-repository \
+  "deb [arch=amd64] http://apt.kubernetes.io \
+  kubernetes-$(lsb_release -cs) \
+  main"
 
-#add kubernetes repo to apt sources list
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+#add docker apt repository and gpg key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-#add docker repo to apt sources list
-cat <<EOF > /etc/apt/sources.list.d/docker.list
-deb https://apt.dockerproject.org/repo ubuntu-xenial main
-EOF
+add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
 
 apt-get update && apt-get install -yq \
   kubeadm=$KUBEVER \
   kubectl=$KUBEVER \
-  docker-engine=$DOCKERVER
+  docker-ce=$DOCKERVER
