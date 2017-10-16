79c76292b0fbab18a1b07090f534b1bd184c98ef
Hagai Barel <hagaibarel@gmail.com>
2017-10-16 10:00:45 +0300

    Add more verbosity

--- install.sh
+++ install.sh
@@ -4,12 +4,13 @@ export KUBEADMVER=1.8.1-01
 export KUBECTLVER=1.8.1-00
 export DOCKERVER=17.03.2~ce-0~ubuntu-xenial
 
+echo "running apt-get update and installing some packages"
 apt-get update && apt-get install -yq apt-transport-https \
   linux-image-extra-$(uname -r) \
   linux-image-extra-virtual \
   ca-certificates
 
-#add kubernetes apt repository and gpg key
+echo "add kubernetes apt repository and gpg key"
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 add-apt-repository \
@@ -17,7 +18,7 @@ add-apt-repository \
   kubernetes-$(lsb_release -cs) \
   main"
 
-#add docker apt repository and gpg key
+echo "add docker apt repository and gpg key"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 add-apt-repository \
@@ -25,6 +26,7 @@ add-apt-repository \
   $(lsb_release -cs) \
   stable"
 
+echo "installing kubeadm and dependencies"
 apt-get update && apt-get install -yq \
   kubeadm=$KUBEADMVER \
   kubectl=$KUBECTLVER \
